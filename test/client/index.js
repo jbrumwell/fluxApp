54b8992b7a66380e9e86507c1ae999baa46978f4
Michał Kawalec <michal@bazzle.me>
2015-02-18 15:53:40 +0100

    new fluxapp-router is used

--- test/client/index.js
+++ test/client/index.js
@@ -6,7 +6,6 @@ window.setImmediate = process.nextTick;
 require('../server/fluxapp');
 require('../server/stores');
 require('../server/actions');
-require('../server/router');
 require('../server/dispatcher');
 require('./stores');
 require('./actions');
