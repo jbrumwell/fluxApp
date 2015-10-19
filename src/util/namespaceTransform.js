d0e13f22999c1e387c65a648d16c3bd906e3309e
Jason Brumwell <jason.brumwell@gmail.com>
2015-10-19 11:28:26 -0400

    support camelcase action names

--- src/util/namespaceTransform.js
+++ src/util/namespaceTransform.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 export default (namespace, suffix) => {
+  namespace = namespace.toLowerCase();
   namespace = suffix ? `${namespace}:${suffix}` : namespace;
 
   return _.snakeCase(namespace).toUpperCase();
